989d7dd2fb50f3c54c2561fdea1abb48594927f7
zhengyangju <m_git_repo@petalmail.com>
2023-08-02 19:57:17 +0800

    fix:下载网红模版 请求问题处理

--- src/views/celebrities/download/index.vue
+++ src/views/celebrities/download/index.vue
@@ -8,8 +8,7 @@ import { downloadCelebrityFiles } from '@/api/modules/celebrity';
 
 const downloadCelebrities = () => {
     downloadCelebrityFiles()
-        .then(({ data, headers }) => {
-            console.log(data);
+        .then(({ response, headers }) => {
             let filename = null;
             if (headers['content-disposition'].indexOf('filename*=') > 0) {
                 filename = decodeURIComponent(headers['content-disposition'].split('filename*=')[1].split("''")[1]);
@@ -17,7 +16,7 @@ const downloadCelebrities = () => {
                 filename = headers['content-disposition'].split(';')[1].split('=')[1];
             }
             const downloadUrl = window.URL.createObjectURL(
-                new Blob([data], { type: 'application/vnd.ms-excel;charset=utf-8' }),
+                new Blob([response.data], { type: 'application/vnd.ms-excel;charset=utf-8' }),
                 // new Blob([data], {
                 //     type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8',
                 // }),
